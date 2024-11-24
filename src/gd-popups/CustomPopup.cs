34676a1d8a1d24c5a53cb3265ad3749f09a4b906
Artem Votintsev <artem.votincev@gmail.com>
2024-11-24 12:23:52 +0100

    Comment 3.6 changes

--- src/gd-popups/CustomPopup.cs
+++ src/gd-popups/CustomPopup.cs
@@ -49,9 +49,10 @@ public partial class CustomPopup
         this.closeButton.Connect(CommonSignals.Pressed, this, nameof(Close));
         Title = title;
         ShowCloseButton = showCloseButton;
-
-        this.GetTree().EditedSceneRoot?.SetEditableInstance(this, true);
-        this.SetDisplayFolded(true);
+#if DEBUG
+       this.GetTree().EditedSceneRoot?.SetEditableInstance(this, true);
+       this.SetDisplayFolded(true);
+#endif
     }
 
     protected void Close()
